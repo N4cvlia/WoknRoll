6b37060df5603397debee63818315f8a06b01525
Nacvlia <kaxa487@gmail.com>
2025-08-28 23:20:37 +0400

    Project architecture made

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './Pages/home/home.component';
+import { ShopComponent } from './Pages/shop/shop.component';
+import { DetailsComponent } from './Pages/details/details.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: HomeComponent},
+    {path: 'shop', component: ShopComponent},
+    {path: 'cart', loadComponent: () => import('./Pages/cart/cart.component').then(m => m.CartComponent)},
+    {path: 'product/:id', loadComponent: () => import('./Pages/details/details.component').then(m => m.DetailsComponent)},
+    {path: '**', redirectTo: ''}
+];
