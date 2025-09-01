529f3c30e01493ad52b090d169d12a95591c2795
Nacvlia <kaxa487@gmail.com>
2025-09-01 17:53:50 +0400

    Filtration added and made some design touchups

--- src/app/Pages/shop/shop.component.ts
+++ src/app/Pages/shop/shop.component.ts
@@ -15,6 +15,7 @@ export class ShopComponent implements OnInit{
   public choosenSpicness: number = -1;
   public noNuts: any = "";
   public vegOnly: any = "";
+  public isChecked: any = "checked";
 
   constructor(private api : ApiService, private actR: ActivatedRoute) {
     scrollTo(0,0);
@@ -30,7 +31,7 @@ export class ShopComponent implements OnInit{
   getAllProducts() {
     this.api.getAllProducts().subscribe((res) => {
       this.allProducts = res;
-      console.log(res);
+      this.isChecked = "checked";
     });
   }
   getAllCategorys() {
@@ -42,7 +43,35 @@ export class ShopComponent implements OnInit{
   getProductByCategory(id: number) {
     this.api.getProductsByCategory(id).subscribe((res: any) => {
       this.allProducts = res.products;
-      console.log(res);
+      this.isChecked = "";
     });
+  }
+  applyFilter() {
+    this.api.getAllProducts().subscribe((res: any) => {
+      this.allProducts = res;
+
+      if(this.choosenSpicness == -1) {
+        this.getAllProducts();
+        this.noNuts = "";
+        this.vegOnly = "";
+      }
+      if(this.choosenSpicness != -1) {
+        this.allProducts = this.allProducts.filter((p: any) => p.spiciness == this.choosenSpicness);
+        this.isChecked = "checked";
+      }
+      if(this.noNuts) {
+        this.allProducts = this.allProducts.filter((p: any) => p.nuts == true);
+      }
+      if(this.vegOnly) {
+        this.allProducts = this.allProducts.filter((p: any) => p.vegeterian == true);
+      }
+      console.log(this.allProducts);
+    });
+  }
+  applyFilterReset() {
+    this.choosenSpicness = -1;
+    this.noNuts = "";
+    this.vegOnly = "";
+    this.getAllProducts();
   }
 }
